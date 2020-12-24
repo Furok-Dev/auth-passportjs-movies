9807b4b8b125c659d3bfea8d217a09ea2fdb0485
Alfredo Aguirre <thefurok@gmail.com>
2020-12-24 05:04:03 -0500

    add new conf for jwt,apikey and default password

--- config/index.js
+++ config/index.js
@@ -3,16 +3,21 @@
  * para las variables de entorno
  */
 
-require("dotenv").config();
+require('dotenv').config();
 
 const config = {
-  dev: process.env.NODE_ENV !== "production",
+  dev: process.env.NODE_ENV !== 'production',
   port: process.env.PORT || 3000,
   cors: process.env.CORS,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
+  defaultUserPassword: process.env.DEFAULT_USER_PASSWORD,
+  defaultAdminPassword: process.env.DEFAULT_ADMIN_PASSWORD,
+  authJwtSecret: process.env.AUTH_JWT_SECRET,
+  publicApiKey: process.env.PUBLIC_API_KEY,
+  adminApiKey: process.env.ADMIN_API_KEY,
 };
 
 module.exports = { config };
