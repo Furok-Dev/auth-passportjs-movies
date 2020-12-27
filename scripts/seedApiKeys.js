b062fc890207ea9fc23ad86bd34912b462657fd7
Alfredo Aguirre <thefurok@gmail.com>
2020-12-27 01:42:11 -0500

    script for add hashed apikeys to our database

---
+++ scripts/seedApiKeys.js
@@ -0,0 +1,68 @@
+/**
+ * Script for create API KEYS
+ *
+ * Execute with: DEBUG= app:* node scripts/seedApiKeys.js
+ */
+
+const chalk = require('chalk');
+const crypto = require('crypto');
+const debug = require('debug')('app:scripts:api-keys');
+const MongoLib = require('../lib/mongo-db');
+
+const adminScopes = [
+  'signin:auth',
+  'signup:auth',
+  'read:movies',
+  'create:movies',
+  'update:movies',
+  'delete:movies',
+  'read:user-movies',
+  'create:user-movies',
+  'delete:user-movies',
+];
+
+const publicScopes = [
+  'signin:auth',
+  'signup:auth',
+  'read:movies',
+  'read:user-movies',
+  'create:user-movies',
+  'delete:user-movies',
+];
+
+const apiKeys = [
+  {
+    token: generateRandomToken(),
+    scopes: adminScopes,
+  },
+  {
+    token: generateRandomToken(),
+    scopes: publicScopes,
+  },
+];
+
+function generateRandomToken() {
+  const buffer = crypto.randomBytes(32);
+  return buffer.toString('hex');
+}
+
+async function seedApiKeys() {
+  try {
+    const mongoDB = new MongoLib();
+
+    const promises = apiKeys.map(async (apiKey) => {
+      await mongoDB.create('apiKeys', apiKey);
+    });
+
+    await Promise.all(promises);
+    debug(
+      chalk.green(`${promises.length} apikeys have been created succesfuly`)
+    );
+    return process.exit(0);
+  } catch (err) {
+    debug(chalk.red(err));
+    process.exit(1);
+  }
+}
+
+seedApiKeys();
